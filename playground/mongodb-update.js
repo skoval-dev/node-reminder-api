2a55ebf9e7246b90eea1449d94a5001032b5abb7
Serhii Koval <dev.skoval@gmail.com>
2017-11-21 00:01:04 +0200

    Removed comments from code

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,78 +7,78 @@ MongoClient.connect('mongodb://localhost:27017/Todo_app', (err, db) => {
     console.log('Connected to MongoDB server');
 
     //findAndUpdate
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID('5a1336ca0b0d4d2e74bf0a6d')
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // }).then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //     console.log(err);
-    // });
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('5a1336ca0b0d4d2e74bf0a6d')
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
 
     //updateMany
-    // db.collection('Todos').updateMany({
-    //     completed: true
-    // }, {
-    //     $set: {
-    //         completed: false
-    //     }
-    // }).then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //    console.log(err);
-    // });
+    db.collection('Todos').updateMany({
+        completed: true
+    }, {
+        $set: {
+            completed: false
+        }
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+       console.log(err);
+    });
 
     //updateOne
-    // db.collection('Todos').updateOne({
-    //     title: 'First to do'
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // }).then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //     console.log(err);
-    // })
+    db.collection('Todos').updateOne({
+        title: 'First to do'
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
 
     //Find and update user instance
-    // db.collection('Users').findOneAndUpdate({
-    //     location: 'Istanbul'
-    // }, {
-    //     $set: {
-    //         name: "Mike",
-    //         age: 22
-    //     }
-    // }, {
-    //     returnOriginal: true
-    // }).then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //     console.log(err);
-    // });
+    db.collection('Users').findOneAndUpdate({
+        location: 'Istanbul'
+    }, {
+        $set: {
+            name: "Mike",
+            age: 22
+        }
+    }, {
+        returnOriginal: true
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
 
 
     //Find and update user's age ($inc) and name
-    // db.collection('Users').findOneAndUpdate({
-    //     name: 'Mike'
-    // }, {
-    //     $set: {
-    //         name: 'Serhii'
-    //     },
-    //     $inc: {
-    //         age: 1
-    //     }
-    // }, {
-    //     returnOriginal: false
-    // }).then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //     console.log(err);
-    // });
+    db.collection('Users').findOneAndUpdate({
+        name: 'Mike'
+    }, {
+        $set: {
+            name: 'Serhii'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
 
     //db.close();
 });
