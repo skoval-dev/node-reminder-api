c0860432e0b6c037970287865aa1e927a05edc15
Serhii Koval <dev.skoval@gmail.com>
2017-11-24 20:15:21 +0200

    Remove comments inside playground files

--- playground/hashing.js
+++ playground/hashing.js
@@ -4,52 +4,52 @@ const bcrypt = require('bcryptjs');
 
 let password = '123abc!';
 
-// let hash = bcrypt.genSalt(10, (err, salt) => {
-//     console.log(salt);
-//     bcrypt.hash(password, salt, (err, hash) => {
-//         console.log(hash);
-//     });
-// });
+let hash = bcrypt.genSalt(10, (err, salt) => {
+    console.log(salt);
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
 let hashed_pass = '$2a$10$RtW.CoVXGnHrpPkiCMSOheNpjzJ2hIqwlOawP8DWw2xmoaxdZTz9S';
 bcrypt.compare(password, hashed_pass, (err, res) => {
    console.log(res);
 });
 
-// let message = "I am user number 3";
-// let hash = SHA256(message).toString();
-//
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
+let message = "I am user number 3";
+hash = SHA256(message).toString();
 
-// let data = {
-//     id: 4,
-// };
-//
-// let token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'somesecret')
-// };
-//
-//   //token.data.id = 5;
-//   //token.hash = SHA256.encrypt(JSON.stringify((token.data))).toString();
-//
-//
-// let result_hash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// if(result_hash === token.hash){
-//     console.log("Data was not change");
-// } else {
-//   console.log("Data was changed. Not trust.")
-// }
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
 
-// let data = {
-//   id: 10,
-//     _id: 2
-// };
-//
-// let token = jwt.sign(data, '123abc');
-// let verify = jwt.verify(token, '123abc');
-// console.log(verify);
+data = {
+    id: 4,
+};
+
+token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'somesecret')
+};
+
+  //token.data.id = 5;
+  //token.hash = SHA256.encrypt(JSON.stringify((token.data))).toString();
+
+
+let result_hash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+if(result_hash === token.hash){
+    console.log("Data was not change");
+} else {
+  console.log("Data was changed. Not trust.")
+}
+
+data = {
+  id: 10,
+    _id: 2
+};
+
+token = jwt.sign(data, '123abc');
+verify = jwt.verify(token, '123abc');
+console.log(verify);
 
 
