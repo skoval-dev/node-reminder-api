cc9be684e6975b73c2280f4d766d7e877e0894cd
Serhii Koval <dev.skoval@gmail.com>
2017-11-29 21:02:15 +0200

    Add async-await to authentication middleware

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,21 +1,21 @@
 const {User} = require('./../models/user');
 
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
     const token = req.header('x-auth');
     if(!token){
         return res.status(401).send({message: "You are not authorised!", success: false});
     }
-    User.find_by_token(token).then((user) => {
+    try{
+        const user = await User.find_by_token(token);
         if(!user){
-            return Promise.reject('You are not authorised!');
+            throw new Error('You are not authorised!');
         }
-
         req.user = user;
         req.token = token;
         next();
-    }).catch((err) => {
-        res.status(401).send({message: err, success: false});
-    });
+    } catch (e) {
+        res.status(401).send({message: e, success: false});
+    }
 };
 
 module.exports = {authenticate};
